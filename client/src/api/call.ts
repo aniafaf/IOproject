ffd88ceb1f26c06d5f00ccd9155d8dbaa429cf97
LaMavia <tosurowiec@gmail.com>
2023-03-18 00:30:00 +0100

    add initial api helpers

---
+++ client/src/api/call.ts
@@ -0,0 +1,15 @@
+import { API_URL } from './constants'
+import { ApiCall } from './types'
+
+/**
+ * A utility wrapper around `fetch` for API calls.
+ */
+export const fetch_api = <T = unknown, O extends Object = Object, E = string>(
+  path: string,
+  body?: O,
+): ApiCall<T, E> =>
+  fetch(`${API_URL}/${path}`, {
+    method: 'POST',
+    credentials: 'include',
+    ...(body ? { body: JSON.stringify(body, null, 0) } : {}),
+  }).then(r => r.json() as ApiCall<T, E>)
