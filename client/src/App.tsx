a1bf4c3d36fffdc4a559633ce567202d66ee9dec
LaMavia <tosurowiec@gmail.com>
2023-03-17 18:20:35 +0100

    format files with prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,19 +4,18 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-      </div>
+    <div className='App'>
+      <div></div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div className='card'>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className='read-the-docs'>
         Click on the Vite and React logos to learn more
       </p>
     </div>
