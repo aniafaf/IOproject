c85fbe6a9ec38744af23681c067dc4a03224ec62
LaMavia <tosurowiec@gmail.com>
2023-03-21 12:41:31 +0100

    fix env add test env

--- client/src/env.d.ts
+++ client/src/env.d.ts
@@ -1,8 +1,11 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
+declare interface ImportedMetaEnv {
+  readonly VITE_ASSETS_BASE: string
   readonly VITE_API_BASE: string
 }
+
+interface ImportMetaEnv extends ImportedMetaEnv {}
 
 interface ImportMeta {
   readonly env: ImportMetaEnv
