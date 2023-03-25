fd4bd37f39ae5df5f3955b96f3d3e0095d11a2a9
LaMavia <tosurowiec@gmail.com>
2023-03-25 16:32:32 +0100

    [ph] add preview to the / route
    Signed-off-by: LaMavia <tosurowiec@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,12 @@ import { Typography, createTheme, ThemeProvider, Grid } from '@mui/material'
 import { StrictMode } from 'react'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
 import { CenterSplitLayout } from './components/CenterSplitLayout'
+import { FieldSet } from './components/FieldSet'
+import { FormButton } from './components/FormButton'
+import { FormHeading } from './components/FormHeading'
+import { FormLink } from './components/FormLink'
 import { Spinner } from './components/Spinner'
+import { TextInputField } from './components/TextInputField'
 import { NotFound } from './views/404'
 
 function App() {
@@ -13,7 +18,30 @@ function App() {
       children: [
         {
           path: '/',
-          Component: () => <CenterSplitLayout>[Placeholder]</CenterSplitLayout>,
+          Component: () => (
+            <CenterSplitLayout>
+              <FormHeading
+                title={`Welcome to _our_name_`}
+                subTitle={`Manage all your finances in one place.`}
+              />
+              <FieldSet>
+                <TextInputField
+                  label='[PH]email'
+                  onUpdate={() => {}}
+                  validate={_ => false}
+                />
+                <TextInputField
+                  label='[PH]password'
+                  type={'password'}
+                  onUpdate={() => {}}
+                  validate={_ => false}
+                />
+                <FormLink>[PH]Forgot password?</FormLink>
+                <FormButton>log in</FormButton>
+                <FormLink align='center'>[PH]Forgot password?</FormLink>
+              </FieldSet>
+            </CenterSplitLayout>
+          ),
         },
       ],
     },
