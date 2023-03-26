e51806c6a7370b5753344ee236ae64607a0c5482
LaMavia <tosurowiec@gmail.com>
2023-03-26 13:56:16 +0200

    add Route class
    Signed-off-by: LaMavia <tosurowiec@gmail.com>

---
+++ client/src/routes.ts
@@ -0,0 +1,7 @@
+export class Route {
+  static home = () => '/'
+  static login = () => '/login'
+  static sign_up = () => '/sign-up'
+  static activate = () => '/activate'
+  static recovery = () => '/recovery'
+}
