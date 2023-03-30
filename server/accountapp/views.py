b01f9bbe22aff1c27fd0d7fbaae03a26d663586c
LaMavia <tosurowiec@gmail.com>
2023-03-30 15:44:20 +0200

    catch login request when method!=post
    Signed-off-by: LaMavia <tosurowiec@gmail.com>

--- server/accountapp/views.py
+++ server/accountapp/views.py
@@ -22,78 +22,97 @@ from accountapp.token import account_activation_token
 
 
 def login_to(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = json.loads(request.body)
         try:
             if "username" not in form:
-                raise ValueError(f'Form lacks username field.')
-            username = form['username']
+                raise ValueError(f"Form lacks username field.")
+            username = form["username"]
             if validation.validate_username(username):
                 if "password" not in form:
-                    raise ValueError(f'Form lacks password field.')
-                password = form['password']
+                    raise ValueError(f"Form lacks password field.")
+                password = form["password"]
                 user = authenticate(request, username=username, password=password)
                 if user is not None:
                     login(request, user)
-                    return JsonResponse({'ok': True, 'error': None, 'data': True})
+                    return JsonResponse({"ok": True, "error": None, "data": True})
                 else:
-                    return JsonResponse({'ok': False, 'error': 'Invalid username or password', 'data': None})
+                    return JsonResponse(
+                        {
+                            "ok": False,
+                            "error": "Invalid username or password",
+                            "data": None,
+                        }
+                    )
 
         except ValueError as e:
-            return JsonResponse({'ok': False, 'error': str(e), 'data': None})
+            return JsonResponse({"ok": False, "error": str(e), "data": None})
+    else:
+        return JsonResponse(
+            {
+                "ok": False,
+                "error": f"Invalid method: expected POST but got {request.method}",
+                "data": None,
+            }
+        )
 
 
-@login_required(login_url='login')
+@login_required(login_url="login")
 def logout_my(request):
     logout(request)
-    return JsonResponse({'ok': True, 'error': None, 'data': True})
+    return JsonResponse({"ok": True, "error": None, "data": True})
 
 
 # TODO moze warto bedzie to przeniesc do jakiegos osobnego pliku?
 def create_my_user(form):
-    email = form['email']
-    username = form['username']
-    password = form['password']
-    first_name = form['first_name']
-    last_name = form['last_name']
-    user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
-    user.is_active = str(os.environ.get('TEST') == '1')
+    email = form["email"]
+    username = form["username"]
+    password = form["password"]
+    first_name = form["first_name"]
+    last_name = form["last_name"]
+    user = User.objects.create_user(
+        username, email, password, first_name=first_name, last_name=last_name
+    )
+    user.is_active = str(os.environ.get("TEST") == "1")
     user.save()
     return user
 
 
 def signup(request):
-    if request.method == 'POST':
+    if request.method == "POST":
         form = json.loads(request.body)
         try:
             if validation.validate_registration(form):
                 user = create_my_user(form)
                 current_site = get_current_site(request)
-                mail_subject = 'Activation link has been sent to your email id'
-                message = render_to_string('accountapp/account_activation_mail.html', {
-                    'user': user.username,
-                    'domain': current_site.domain,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': account_activation_token.make_token(user),
-                })
-                to_email = form['email']
-                email = EmailMessage(
-                    mail_subject, message, to=[to_email]
+                mail_subject = "Activation link has been sent to your email id"
+                message = render_to_string(
+                    "accountapp/account_activation_mail.html",
+                    {
+                        "user": user.username,
+                        "domain": current_site.domain,
+                        "uid": urlsafe_base64_encode(force_bytes(user.pk)),
+                        "token": account_activation_token.make_token(user),
+                    },
                 )
+                to_email = form["email"]
+                email = EmailMessage(mail_subject, message, to=[to_email])
                 email.send()
-                return JsonResponse({'ok': True, 'error': None, 'data': True})
+                return JsonResponse({"ok": True, "error": None, "data": True})
             else:
-                JsonResponse({'ok': True, 'error': "t", 'data': True})
+                JsonResponse({"ok": True, "error": "t", "data": True})
         except ValueError as e:
-            return JsonResponse({'ok': False, 'error': str(e), 'data': None})
+            return JsonResponse({"ok": False, "error": str(e), "data": None})
         except ValidationError as mail_error:
-            return JsonResponse({'ok': False, 'error': str(mail_error), 'data': None})
+            return JsonResponse({"ok": False, "error": str(mail_error), "data": None})
         # TODO to lapie wszystkie errory gdzie coś koliduje z bazą danych, sprobuje to poprawic, żeby tyczyło to tylko
         # powtarzajacego sie username
         except IntegrityError:
-            return JsonResponse({'ok': False, 'error': 'Username already exist', 'data': None})
+            return JsonResponse(
+                {"ok": False, "error": "Username already exist", "data": None}
+            )
         except SMTPException as smtpe:
-            return JsonResponse({'ok': False, 'error': str(smtpe), 'data': None})
+            return JsonResponse({"ok": False, "error": str(smtpe), "data": None})
 
 
 def test(request):
@@ -105,15 +124,24 @@ def activate(request, uid, token):
     try:
         uid = force_str(urlsafe_base64_decode(uid))
         user = user_model.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, user_model.DoesNotExist):
+    except (TypeError, ValueError, OverflowError, user_model.DoesNotExist):
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
         user.save()
-        return JsonResponse({'ok': True, 'error': None, 'data': "Thank you for your email confirmation. "
-                                                                "Now you can log in to your account."})
+        return JsonResponse(
+            {
+                "ok": True,
+                "error": None,
+                "data": "Thank you for your email confirmation. "
+                "Now you can log in to your account.",
+            }
+        )
     else:
-        return JsonResponse({'ok': False, 'error': "Activation link is invalid!", 'data': None})
+        return JsonResponse(
+            {"ok": False, "error": "Activation link is invalid!", "data": None}
+        )
+
 
 # TODO jak zdazymy
 # def password_reset_request(request):
