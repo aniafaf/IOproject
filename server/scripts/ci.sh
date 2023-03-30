4b8272715f011b66864b695e7c0bc505d2460d22
LaMavia <tosurowiec@gmail.com>
2023-03-30 15:50:45 +0200

    add server ci script
    Signed-off-by: LaMavia <tosurowiec@gmail.com>

---
+++ server/scripts/ci.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")" || exit 1
+source ../../scripts/helpers.sh || exit 1
+
+print_info "Formatting files..."
+if black ..**/*.py; then
+  print_ok "Succesfully formatted files."
+else
+  print_error "Failed to format files."
+fi 
