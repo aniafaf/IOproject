b39e16d89fd055a537d11aad080675b7c529ce87
ci <ts438730@students.mimuw.edu.pl>
2023-03-30 18:10:44 +0200

    add test ci
    Signed-off-by: ci <ts438730@students.mimuw.edu.pl>

--- client/scripts/test.sh
+++ client/scripts/test.sh
@@ -6,15 +6,15 @@ cd .. || exit 1
 
 ../server/scripts/dev.sh &
 
-# server_pid=$!
+server_pid=$!
 
 export OP_FINAL="lsof -i :8000 | awk 'NR>1{print \$2}' | xargs kill"
 eval "$OP_FINAL"
 
-# op "Running client tests ($server_pid)..." \
-#   "npm run test-script" \
-#   "Succesfully ran client tests." \
-#   "Failed to run client tests."
+op "Running client tests ($server_pid)..." \
+  "npm run test-script" \
+  "Succesfully ran client tests." \
+  "Failed to run client tests." || exit 1
 
 op "Generating client coverage..." \
   "npm run coverage" \
