97b6297a9eb5bfcea79b589863b7d8039f804d3c
ci <ts438730@students.mimuw.edu.pl>
2023-03-30 19:15:18 +0200

    update tests; split ci jobs
    Signed-off-by: ci <ts438730@students.mimuw.edu.pl>

--- client/src/__tests__/env.test.ts
+++ client/src/__tests__/env.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from 'vitest'
 
 describe('env', () => {
   const expected_env: ImportedMetaEnv = {
-    VITE_API_BASE: '0.0.0.0:8000/api',
+    VITE_API_BASE: 'localhost:8000/api',
     VITE_ASSETS_BASE: '/',
   }
 
