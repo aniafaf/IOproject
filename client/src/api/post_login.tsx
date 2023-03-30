6e366189bcfaa0db9887aa588649cd8d4a69f66d
LaMavia <tosurowiec@gmail.com>
2023-03-30 15:42:29 +0200

    simplify post_login
    Signed-off-by: LaMavia <tosurowiec@gmail.com>

--- client/src/api/post_login.tsx
+++ client/src/api/post_login.tsx
@@ -1,11 +1,8 @@
 import { fetch_api } from './call'
 
-export const post_login = async (username: string, password: string) => {
-  const response = await fetch_api({
+export const post_login = (username: string, password: string) =>
+  fetch_api({
     path: 'login/',
     method: 'POST',
     body: { username, password },
   })
-  return response;
-}
-
