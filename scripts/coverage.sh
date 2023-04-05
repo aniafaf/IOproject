1eff55cc3932a5033b884ca2e34e15fe4ed7b9d1
Zuzanna Surowiec <ts438730@students.mimuw.edu.pl>
2023-04-05 23:05:00 +0200

    add coverage w/o committing on all branches

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -4,13 +4,13 @@ cd "$(dirname "$0")" || exit 1
 source ./helpers.sh || exit 1
 
 op "Initialising CI env..." \
-    "./init.sh" \
-    "Succesfully initialised CI env." \
-    "Failed to initialise CI env." 
+  "./init.sh" \
+  "Succesfully initialised CI env." \
+  "Failed to initialise CI env."
 
 print_info "Running client coverage..."
 cd ../client || exit 1
-if ./scripts/coverage.sh; then 
+if ./scripts/coverage.sh; then
   print_ok "Successfully ran client CI."
 else
   print_error "Failed to run client CI."
@@ -23,15 +23,22 @@ cd .. || exit 1 # return to the root folder
 # op "Running server CI..." \
 #     "./server/scripts/coverage.sh" \
 #     "Successfully ran server CI." \
-#     "Failed to run server CI." 
+#     "Failed to run server CI."
 
 if [[ -n "$DEBUG" ]]; then
   exit $!
 fi
 
 # Exit early if the head is detached
-if ! git symbolic-ref -q HEAD; then
+if is_detached; then
+  print_info "Exiting without committing because in detached head"
   exit $!
+fi
+
+current_branch=$(branch_name)
+if [ "$current_branch" != "main" ]; then
+  print_info "Exiting without committing to branch '$current_branch'"
+  exit 0
 fi
 
 print_info "Committing above changes."
@@ -44,5 +51,5 @@ git branch --set-upstream-to=origin gh-pages
 git add -A
 git add -f ./client/coverage/*
 git pull origin/gh-pages
-git commit -m "CI [$(date -u +"%Y.%m.%d %T")]"
+git commit -m "Coverage [$(date -u +"%Y.%m.%d %T")]"
 git push -f -u origin gh-pages
