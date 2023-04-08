315851fb0e06f0be0c9509647c0e97b276a5a0d1
aktowon <maciek.nowotek@gmail.com>
2023-04-08 19:05:37 +0200

    add event model

--- server/accountapp/models.py
+++ server/accountapp/models.py
@@ -16,3 +16,16 @@ class UserGroup(models.Model):
 
     def __str__(self):
         return str(self.user.username) + " " + str(self.group.name)
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.group) + ": " + str(self.name)
+
+    def save(self, *args, **kwargs):
+        if self.group.event_set.count() > 0:
+            raise ValueError
+        super(Event, self).save(*args, **kwargs)
