2c809a615a4d1b88abcc3c71e4664c9dfadcaebe
aktowon <maciek.nowotek@gmail.com>
2023-04-08 18:56:23 +0200

    add user-group relation model

--- server/accountapp/models.py
+++ server/accountapp/models.py
@@ -9,3 +9,10 @@ class Group(models.Model):
     def __str__(self):
         return str(self.name)
 
+
+class UserGroup(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.user.username) + " " + str(self.group.name)
