0ce6ab069c088d71fab4597d0cf650745020cfcc
aktowon <maciek.nowotek@gmail.com>
2023-04-08 18:48:53 +0200

    add group model

--- server/accountapp/models.py
+++ server/accountapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Group(models.Model):
+    name = models.CharField(max_length=30, blank=False)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
+
