3677a02a1d1eed3e8d0a020374a7329ae112b926
aktowon <maciek.nowotek@gmail.com>
2023-04-10 18:26:40 +0200

    add urls for groups

--- server/accountapp/urls.py
+++ server/accountapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("login/", views.login_to, name="login"),
     path("signup/", views.signup, name="signup"),
     path("activate/", views.activate, name="activate"),
+    path("group/<int:pk>/", views.group_selected, name="group_selected"),
+    path("grpup_list", views.group_list, name="group_list"),
 ]
 
 if isTest:
