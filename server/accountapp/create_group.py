82f91aed64488f176ae38c4ac00cf169a758f89d
aktowon <maciek.nowotek@gmail.com>
2023-04-10 18:24:52 +0200

    add file for creating group validation (todo)

---
+++ server/accountapp/create_group.py
@@ -0,0 +1,12 @@
+from models import Group, UserGroup
+
+
+def validate_group(form):
+    pass
+
+
+def create_group(user, name):
+    group = Group(admin=user, name=name)
+    group.save()
+    connection = UserGroup(user=user, group=group)
+    connection.save()
