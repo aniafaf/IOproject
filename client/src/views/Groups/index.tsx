835879d91064090fc1fd9f63dad176c6cc73bc98
Zuzanna Surowiec <ts438730@students.mimuw.edu.pl>
2023-04-10 23:26:18 +0200

    simplify .then if

--- client/src/views/Groups/index.tsx
+++ client/src/views/Groups/index.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
-import { get_group_list, GroupListItem } from '../../api/groups'
+import { get_group_list, Group } from '../../api/groups'
 import { LoggedInGuard } from '../../components/LoggedInGuard'
 import { Spinner } from '../../components/Spinner'
 import { useAlert } from '../../hooks/alert'
@@ -8,19 +8,15 @@ import { Route } from '../../routes'
 import { GroupListItemElement } from './components/GroupListItemElement'
 
 export const GroupListView = () => {
-  const [groups, setGroups] = useState<GroupListItem[]>([])
+  const [groups, setGroups] = useState<Group[]>([])
   const [loading, setLoading] = useState(true)
   const alert = useAlert()
 
   useEffect(() => {
     get_group_list()
-      .then(r => {
-        if (r.ok) {
-          setGroups(r.data)
-        } else {
-          alert.display(r.error, 'error')
-        }
-      })
+      .then(r =>
+        r.ok ? setGroups(r.data.groups) : alert.display(r.error, 'error'),
+      )
       .catch(e => alert.display(e, 'error'))
       .finally(() => setLoading(false))
   }, [])
