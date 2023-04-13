fe44cd2a645b09a24f5911ecb20daf39f8845db3
aktowon <maciek.nowotek@gmail.com>
2023-04-13 23:57:11 +0200

    validation

--- server/accountapp/group.py
+++ server/accountapp/group.py
@@ -1,15 +1,25 @@
 from .models import Group, UserGroup
 
 
-def validate_group(form):
+def validate_existing_group(form):
     if "group_id" not in form:
-        raise ValueError(f"Form lacks group_id field")
+        raise ValueError("Form lacks group_id field")
     group_id = form["group_id"]
     if not Group.objects.filter(id=group_id).exists():
-        raise ValueError(f"Group doesn't exits")
+        raise ValueError("Group doesn't exits")
 
 
-def create_group(user, name):
+def validate_new_group(form):
+    if "name" not in form:
+        raise ValueError("Form lacks name field")
+    name = form["name"]
+    if name.len > 30 or name.len == 0:
+        raise ValueError("Group name must contains between 1 to 30 characters")
+
+
+def create_group(user, form):
+    validate_new_group(form)
+    name = form["name"]
     group = Group(admin=user, name=name)
     group.save()
     connection = UserGroup(user=user, group=group)
