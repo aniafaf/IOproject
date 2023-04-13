f4591f3ab26965697b879cc77725aa58b4719f0f
aktowon <maciek.nowotek@gmail.com>
2023-04-14 00:28:59 +0200

    add hash

--- server/accountapp/group.py
+++ server/accountapp/group.py
@@ -1,9 +1,11 @@
+import random
+
 from .models import Group, UserGroup
 
 
 def validate_existing_group(form):
     if "group_id" not in form:
-        raise ValueError("Form lacks group_id field")
+        raise ValueError("Form lacks group_id field.")
     group_id = form["group_id"]
     if not Group.objects.filter(id=group_id).exists():
         raise ValueError("Group doesn't exits")
@@ -11,16 +13,17 @@ def validate_existing_group(form):
 
 def validate_new_group(form):
     if "name" not in form:
-        raise ValueError("Form lacks name field")
+        raise ValueError("Form lacks name field.")
     name = form["name"]
     if name.len > 30 or name.len == 0:
-        raise ValueError("Group name must contains between 1 to 30 characters")
+        raise ValueError("Group name must contains between 1 to 30 characters.")
 
 
 def create_group(user, form):
     validate_new_group(form)
     name = form["name"]
-    group = Group(admin=user, name=name)
+    hash = random.randint(10000000, 99999999)
+    group = Group(admin=user, name=name, hash=hash)
     group.save()
     connection = UserGroup(user=user, group=group)
     connection.save()
@@ -29,6 +32,12 @@ def create_group(user, form):
 def add_to_group(user, form):
     validate_existing_group(form)
     group_id = form["group_id"]
-    group = Group.objects.get(pk=group_id)
+    if "hash" not in form:
+        raise ValueError("Form lacks hash field.")
+    hash = form["hash"]
+    try:
+        group = Group.objects.get(pk=group_id, hash=hash)
+    except Group.DoesNotExist:
+        raise ValueError("Given link is not correct.")
     connection = UserGroup(user=user, group=group)
     connection.save()
