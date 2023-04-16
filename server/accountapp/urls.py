ade45703a55a6feccb7fe22159273646f78fafdf
aktowon <maciek.nowotek@gmail.com>
2023-04-16 22:35:20 +0200

    refractor files

--- server/accountapp/urls.py
+++ server/accountapp/urls.py
@@ -1,26 +1,26 @@
 from django.urls import path
-from . import views
+from . import register_views, group_views
 import os
 
 isTest = os.environ.get("TEST") == "1"
 
 urlpatterns = [
-    path("login/", views.login_to, name="login"),
-    path("signup/", views.signup, name="signup"),
-    path("activate/", views.activate, name="activate"),
-    path("group/<int:pk>/", views.group_selected, name="group_selected"),
-    path("group_list/", views.group_list, name="group_list"),
-    path("create_group/", views.create_group, name="create_group"),
-    path("join/", views.join_group, name="join_group"),
-    path("logout/", views.logout_my, name="logout"),
+    path("login/", register_views.login_to, name="login"),
+    path("signup/", register_views.signup, name="signup"),
+    path("activate/", register_views.activate, name="activate"),
+    path("group/<int:pk>/", group_views.group_selected, name="group_selected"),
+    path("group_list/", group_views.group_list, name="group_list"),
+    path("create_group/", group_views.create_group, name="create_group"),
+    path("join/", group_views.join_group, name="join_group"),
+    path("logout/", register_views.logout_my, name="logout"),
 ]
 
 if isTest:
     urlpatterns.extend(
         [
-            path("delete-all/", views.delete_all, name="delete-all"),
+            path("delete-all/", register_views.delete_all, name="delete-all"),
             path(
-                "delete-all-groups/", views.delete_all_groups, name="delete-all-groups"
+                "delete-all-groups/", group_views.delete_all_groups, name="delete-all-groups"
             ),
         ]
     )
