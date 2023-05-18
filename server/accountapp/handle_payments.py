a4dabdccf321e74df3ef5020d59ff83225d8a96a
aktowon <maciek.nowotek@gmail.com>
2023-05-19 00:42:52 +0200

    add payment creation method without validation

---
+++ server/accountapp/handle_payments.py
@@ -0,0 +1,34 @@
+from .models import Group, Payment, Event, Debtor
+from django.contrib.auth.models import User
+
+
+# TODO
+def validate_new_element(form):
+    pass
+
+
+def create_payment(user, form, pk_e):
+    validate_new_element(form)
+    name = form["name"]
+    amount = form["amount"]
+    category = form["category"]
+    event = Event.objects.get(id=pk_e)
+    payment = Payment(name=name, amount=amount, category=category, lender=user, event=event)
+    if "description" in form:
+        payment.description = form["description"]
+    payment.save()
+
+    users_id = form["users_id"]
+    even = form["even"] in form
+    if even:
+        even_split = amount / len(users_id)
+        for id in users_id:
+            user = User.objects.get(id=id)
+            debtor = Debtor(user=user, payment=payment, amount=even_split)
+            debtor.save()
+    else:
+        users_debt = form["users_debt"]
+        for (id, debt) in zip(users_id, users_debt):
+            user = User.objects.get(id=id)
+            debtor = Debtor(user=user, payment=payment, amount=debt)
+            debtor.save()
---
+++ server/accountapp/views_payment.py
@@ -0,0 +1,32 @@
+import json
+import os
+from .models import Group, Event
+from .constructors.session_guard import session_guard
+from .constructors.api_response import (
+    ok_response,
+    error_response,
+    session_expired_response,
+)
+from . import handle_payments
+
+
+@session_guard
+def create_payment(request, pk_g, pk_e):
+    group = Group.objects.get(pk_g)
+    user_list = group.members.all()
+    user = request.user
+    if user not in user_list:
+        return error_response("You are not in this group")
+
+    if request.method == "POST":
+        try:
+            form = json.loads(request.body)
+            handle_payments.create_payment(user, form, pk_e)
+            return ok_response(True)
+        except ValueError as e:
+            return error_response(str(e))
+
+    user_list = list(
+        user_list.values("id", "username", "first_name", "last_name", "email")
+    )
+    return ok_response({"users": user_list})
