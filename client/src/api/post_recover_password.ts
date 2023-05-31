7097a87455751fb63877940e7a3691021a753f89
Paweł Preibisch <pawka1114@gmail.com>
2023-05-31 13:14:20 +0200

    add fetch api for password recover

---
+++ client/src/api/post_recover_password.ts
@@ -0,0 +1,8 @@
+import { fetch_api } from './call'
+
+export const post_recover_password = (password: string, passwordConfirmation: string) =>
+    fetch_api({
+        path: '/',
+        method: 'POST',
+        body: { password, passwordConfirmation },
+    })
