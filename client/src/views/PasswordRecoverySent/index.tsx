9b0184ec067915d7a7740fa48030aa9c2971496e
Paweł Preibisch <pawka1114@gmail.com>
2023-05-31 13:11:52 +0200

    add password recovery sent page

---
+++ client/src/views/PasswordRecoverySent/index.tsx
@@ -0,0 +1,42 @@
+import { CenterSplitLayout } from '../../components/CenterSplitLayout'
+import { FieldSet } from '../../components/FieldSet'
+import { FormButton } from '../../components/FormButton'
+import { FormHeading } from '../../components/FormHeading'
+import { FormLink } from '../../components/FormLink'
+import { NotLoggedInGuard } from '../../components/LoggedInGuard'
+import { TextInputField } from '../../components/TextInputField'
+import { Route } from '../../routes'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { post_recover } from '../../api/post_recover'
+import { useAlert } from '../../hooks/alert'
+import { Spinner } from '../../components/Spinner'
+
+export const PasswordRecoverySentView = () => {
+    const navigate = useNavigate()
+    const alert = useAlert()
+    const [loading, setLoading] = useState(false)
+
+    return (
+        <>
+            <NotLoggedInGuard />
+            <alert.AlertComponent />
+            <Spinner open={loading} />
+            <CenterSplitLayout>
+                <FieldSet>
+                    <FormHeading
+                        title={`Password reset sent`}
+                        subTitle={`We’ve emailed you instructions for setting your password, if an account exists 
+                        with the email you entered. You should receive them shortly. If you don’t receive an email, 
+                        please make sure you’ve entered the address you registered with, and check your spam folder.`}
+                    />
+                </FieldSet>
+                <FieldSet>
+                    <FormLink to={Route.login()} align='center'>
+                        Back to log in
+                    </FormLink>
+                </FieldSet>
+            </CenterSplitLayout>
+        </>
+    )
+}
